d057358172e1b36f1e7c58e010caea03b1fd4aa7
Valotto18 <rm551445@fiap.com.br>
2023-08-21 10:02:35 -0300

    Reinicializando o App.jsx

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+
+
+export default function App() {
+  return (
+    
+    <ul>
+        
+        <li>FIAP</li>
+        <li>Google</li>
+        <li>Meta</li>
+        <li>Aws</li>
+    </ul>
+    
+  )
+}
